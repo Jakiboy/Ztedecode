f68eee647fd13b98b152c3e3dcadc0ced00eda3b
Jakiboy <j.sinnaour.official@gmail.com>
2025-08-02 21:55:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,12 @@
-"""setup.py module installer"""
-
 from setuptools import setup, find_packages
 
-with open("README.md") as f:
-    README = f.read()
-
-with open("LICENSE") as f:
-    LICENSE = f.read()
-
-with open("VERSION") as f:
-    VERSION = f.read().strip()
-
-with open("requirements.txt") as f:
-    REQUIRED = f.read()
-
 setup(
-    name="zcu",
-    version=VERSION,
-    description="ZTE Configuration Utility",
-    long_description=README,
+    name="Ztedecode",
+    version="0.5.3",
+    packages=find_packages(),
+    install_requires=[],
     author="Mark Street",
-    author_email="mkst@protonmail.com",
+    description="ZTE router decode utilities",
     url="https://github.com/mkst/zte-config-utility",
-    license=LICENSE,
-    install_requires=REQUIRED,
-    packages=find_packages(exclude=("tests", "docs")),
+    python_requires=">=3.6",
 )
